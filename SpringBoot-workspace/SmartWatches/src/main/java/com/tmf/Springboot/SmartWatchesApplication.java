e5ff496198035e6249c8f41b759a926010eaa677
geethajoga <145979936+geethajoga@users.noreply.github.com>
2024-01-19 14:25:57 +0530

    Add files via upload

---
+++ SpringBoot-workspace/SmartWatches/src/main/java/com/tmf/Springboot/SmartWatchesApplication.java
@@ -0,0 +1,13 @@
+package com.tmf.Springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartWatchesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartWatchesApplication.class, args);
+	}
+
+}
